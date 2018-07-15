03d0665460f96b0371dcc0991df0dc6ef0de92cb
Joseph Wu <joe@192-168-1-8.tpgi.com.au>
2018-07-15 20:54:00 +1000

    initial commit

---
+++ Algorithms.Test/Algorithms.Test.cs
@@ -0,0 +1,60 @@
+using System;
+using Xunit;
+using AlgorithmsLibrary;
+using System.Linq;
+
+namespace Algorithms.Test
+{
+    public class AlgorithmsLibTest
+    {
+        [Theory]
+        [InlineData(321, 6)]
+        [InlineData(1, 1)]
+        [InlineData(978, 24)]
+        [InlineData(5434, 16)]
+        public void Sum_ShouldProduceSumOfTheDigits(int input, int expected)
+        {
+            // arrange
+            AlgorithmsLib al = new AlgorithmsLib();
+            // act
+            int result = al.sum(input);
+            // assert
+            Assert.True(result == expected);
+        }
+
+        [Theory]
+        [InlineData(new int[]{4, 2, 1}, new int[]{4,2}, new int[]{1})]
+        [InlineData(new int[]{4, 2, 1, 3}, new int[]{4,2}, new int[]{1, 3})]
+        public void SplitIntegerArray_ShouldProduceTwoSubArrays(int[] input, int[] expectedLeft, int[] expectedRight)
+        {
+            AlgorithmsLib al = new AlgorithmsLib();
+            int[] resultLeft = new int[]{};
+            int[] resultRight = new int[]{};
+            al.SplitIntegerArray(input, out resultLeft, out resultRight);
+
+            Assert.True(resultLeft.SequenceEqual(expectedLeft));
+            Assert.True(resultRight.SequenceEqual(expectedRight));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 5}, new int[]{2 ,4, 8}, new int[] {1, 2, 4, 5, 8} )]
+        [InlineData(new int[] { 1, 5, 6, 98}, new int[]{2, 4, 8, 10, 11}, new int[] {1, 2, 4, 5, 6, 8, 10, 11, 98} )]
+        public void MergeTwoSortedList_ShouldMergeSuccessfull(int[] input1, int[] input2,
+            int[] expectedIntegerArray)
+        {
+            AlgorithmsLib al = new AlgorithmsLib();
+            int[] result = al.MergeTwoSortedList(input1, input2);
+            Assert.True(result.SequenceEqual(expectedIntegerArray));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 5,2,76, 1,34}, new int[] { 1, 2,5, 34, 76})]
+        [InlineData(new int[] { 5}, new int[] { 5 })]
+        public void MergeSort_ShouldMergeSortSuccessful(int[] input, int[] expected)
+        {
+            AlgorithmsLib al = new AlgorithmsLib();
+            int[] result = al.MergeSort(input);
+            Assert.True(result.SequenceEqual(expected));
+        }
+    }
+}
---
+++ AlgorithmsLib/AlgorithmsLib.cs
@@ -0,0 +1,311 @@
+using System;
+
+namespace AlgorithmsLibrary
+{
+    public class AlgorithmsLib
+    {
+        //Q1
+        public int sum(int num) {
+            if ( num == 0) 
+                return 0;
+            return num % 10 + sum(num / 10);
+        }
+
+        //Q2
+        public int isArmstrongNumber(int num) {
+            return cubeSum(num);
+        }
+
+        public int cubeSum(int num) {
+            if (num == 0) return 0;
+            return Convert.ToInt32(Math.Pow(num % 10, 3) ) + cubeSum(num / 10);
+        }
+
+        //Q3
+        public bool isPalindrome(string input) {
+            int min = 0;
+            int max = input.Length - 1;
+            while(true) {
+                if (min > max)
+                    return true;
+                char a = input[min];
+                char b = input[max];
+                if (char.ToLower(a) != char.ToLower(b)){
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+        //Q4
+        public string RemoveDuplicatesInString(string input){
+            if(String.IsNullOrEmpty(input)) return null;
+
+            char[] a = new char[input.Length];
+            int c = 0;
+            for (int i = 0; i < input.Length; i++){
+                char current =input[i];
+                bool found = false;
+                foreach(char j in a){
+                    if(j == current){
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    a[c] = current;
+                    c++;
+                }
+            }
+            string newstring = "";
+            foreach(char i in a){               
+                newstring += i;
+            }
+            return newstring.Trim();
+        }
+
+        public string RemoveDuplicatesInString_v2(string input) {
+
+            string result = "";
+
+            foreach(char i in input) {
+                if (result.IndexOf(i) == -1)
+                    result += i;
+            }
+            return result;
+        }
+
+        //Q5
+        public bool areTheyAnagrams(string a1, string a2) {
+            if ( String.IsNullOrEmpty(a1) || String.IsNullOrEmpty(a2))
+                return false;
+            
+            if (a1.Length != a2.Length)
+                return false;
+
+            while(a1.Length >= 1){
+                int location = a2.IndexOf(a1[0]);
+                if (location == -1)
+                    return false;
+                if(a1.Length == 1)
+                    return true;
+                a1 = a1.Remove(0, 1);
+                a2 = a2.Remove(location, 1);
+            }
+
+            return true;
+
+        }
+
+        //Q6: is number a prime number      
+        public bool isPrime(int input){
+            if (input <= 1) return false;
+            int counter = 2;
+            while (counter < input){
+                if(input % counter == 0)
+                    return false;
+                counter++;
+            }
+            return true;
+        }
+
+        //Q7: selection sort. in place sort. not adaptive. not stable
+        public int[] selectionSort (int[] input)
+        {
+            if (input == null)
+                return null;
+            if (input.Length == 1)
+                return input;
+           
+            for (var k = 0; k < input.Length; k++)
+            {              
+                for (var i = k + 1; i < input.Length; i++)
+                {
+                    if (input[i] < input[k])
+                    {
+                        int temp = input[k];
+                        input[k] = input[i];
+                        input[i] = temp;
+                        break;
+                    }
+                }   
+            }
+            return input;
+        }
+        
+
+        // Q8: Bubble sort. O(N^2). stable sort. adaptive. in-place sort
+        public int[] BubbleSort (int[] input)
+        {
+            if (input == null || input.Length == 0) return null;
+
+            while (true)
+            {
+                int swapCount = 0;
+                for (var i = 0; i < input.Length - 1; i++)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        (input[i], input[i + 1]) = (input[i+1], input[i]);
+                        swapCount++;
+                    }
+                }
+                if (swapCount == 0)
+                    return input;
+            }           
+        }
+
+        // Q9: insertion sort. O(N^2). Adaptive. Sort in place.
+        public int[] InsertionSort (int[] input)
+        {
+            if (input == null) return null;
+
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                for (var j = i + 1; j > 0; j--)
+                {
+                    if (input[j] < input[j - 1])
+                    {
+                        (input[j], input[j - 1]) = (input[j - 1], input[j]);
+                    } else 
+                    {
+                        break;
+                    }
+                }
+            }  
+            return input;
+        }
+        
+        public int[] InsersionSort_V2(int[] input, int startIndex, int increment)
+        {
+            if (input == null) return null;
+            if (startIndex < 0 || startIndex > input.Length - 1) return input;
+            if (increment <= 0 || increment > input.Length -1) return input;
+
+            for (var i = startIndex; i < input.Length; i = i + increment)
+            {
+                for (var j = Math.Min(i + increment, input.Length - 1); 
+                    j - increment >= 0; 
+                    j = j - increment)
+                {
+                    if (input[j] < input[j - increment])
+                    {
+                        (input[j], input[j - increment]) = (input[j - increment], input[j]);
+                    } else 
+                    {
+                        break;
+                    }
+                }                
+            }
+            return input;
+            
+        }
+
+        // Q10: Shell Sort. between O(N) and O(N^2). sort in place. adaptive.
+        public int[] ShellSort (int[] input, int increment) 
+        {
+            if (input == null) return null;
+            if (input.Length == 1) return input;
+            if (increment <= 0) return input;
+
+            for (var i = 0; i < increment; i++)
+            {
+                input = InsersionSort_V2 (input, i, increment);
+            }
+            return input = InsersionSort_V2 (input, 0, 1);
+        }
+
+        // Q11: Merge Sort. The order is O(NLogN)
+        public void SplitIntegerArray (int[] input, out int[] output1, out int[] output2)
+        {
+            if (input == null) 
+            {
+                output1 = null;
+                output2 = null;
+                return;
+            }
+            if (input.Length == 1)
+            {
+                output1 = null;
+                output2 = null;
+                return;
+            }
+
+            int splitLeftNo = (input.Length % 2 == 0) ? input.Length / 2 : input.Length /2 + 1;
+
+            output1 = new int[splitLeftNo];
+            output2 = new int[input.Length - splitLeftNo];
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (i < splitLeftNo)
+                {
+                    output1[i] = input[i];
+                } else {
+                    output2[i - splitLeftNo] = input[i];
+                }
+            }
+        }
+
+        public int[] MergeTwoSortedList(int[] input1, int[] input2)
+        {
+            if (input1 == null && input2 == null) return null;
+            if (input1 == null) return input2;
+            if (input2 == null) return input1;
+
+            int count = 0;
+            int[] output = new int[input1.Length + input2.Length];
+            
+            int input1Index = 0;
+            int input2Index = 0;
+            while (input1Index < input1.Length && input2Index < input2.Length)
+            {
+                if (input1[input1Index] < input2[input2Index])
+                {
+                    output[count] = input1[input1Index];
+                    input1Index++;
+                } else if (input2Index < input2.Length) 
+                {
+                    output[count] = input2[input2Index];
+                    input2Index++;
+                }
+                count++;
+            }
+            if (input1Index < input1.Length)
+            {
+                while (count < output.Length)
+                {
+                    output[count++] = input1[input1Index++];
+                }
+            }
+            if (input2Index < input2.Length)
+            {
+                while (count < output.Length)
+                {
+                    output[count++] = input2[input2Index++];
+                }
+            }
+            return output;
+        }
+
+        public int[] MergeSort(int[] input)
+        {
+            if (input == null) return null;
+            if (input.Length == 1) return input;
+
+            int midPointIndex = input.Length /2 + input.Length % 2;
+            int[] leftArray = new int[]{};
+            int[] rightArray = new int[]{};
+            SplitIntegerArray(input, out leftArray, out rightArray);
+
+            leftArray = MergeSort(leftArray);
+            rightArray = MergeSort(rightArray);
+
+            return MergeTwoSortedList(leftArray, rightArray);
+        }
+
+    }
+
+
+}
