0c01bbb2825d7dd4b0bfe69732e0e6cf7e0c53b3
Joseph Wu <wuzhou88@gmail.com>
2018-12-01 22:23:52 +1100

    solve convert number to word subroutine

--- Algorithms.Test/InterviewPreparation.Test.cs
+++ Algorithms.Test/InterviewPreparation.Test.cs
@@ -9,6 +9,96 @@ namespace Algorithms.Test
 {
     public class InterviewPreparationTest
     {
-        
+        [Theory]
+        [InlineData("123", "321")]
+        [InlineData("abc3d", "d3cba")]
+        [InlineData("abc3d 3", "3 d3cba")]
+        public void ReverseString_shouldReturnCorrectString(string input, string output)
+        {
+            //arrange
+            var abc = new InterviewPreparation();
+            //act
+            var expected = abc.ReverseString(input);
+
+            //assert
+            Assert.True(expected == output);
+
+        }
+
+        [Theory]
+        [InlineData(56, 2)]
+        [InlineData(31283, 5)]
+        public void digitCount_ShouldReturnCorrectDigits(int input, int output)
+        {
+            //arrange
+            var abc = new InterviewPreparation();
+            //act
+            var expected = abc.digitCount(input);
+            //assert
+            Assert.True(expected == output);
+
+        }
+
+        [Theory]
+        [InlineData(1, "one")]
+        [InlineData(9, "nine")]
+        [InlineData(0, "zero")]
+        [InlineData(10, "ten")]
+        [InlineData(11, "eleven")]
+        [InlineData(19, "nineteen")]
+        [InlineData(45, "forty five")]
+        [InlineData(99, "ninety nine")]
+        [InlineData(20, "twenty")]
+        [InlineData(100, "one hundred")]
+        [InlineData(102, "one hundred and two")]
+        [InlineData(402, "four hundred and two")]
+        [InlineData(999, "nine hundred ninety nine")]
+        // [InlineData(1234, "one thousand two hundred thirty four")]
+        // [InlineData(1234, "one thousand two hundred thirty four")]
+        public void Turn3digitsIntegerIntoWord_ShouldReturnCorrectWordRepresentation(int input, string output)
+        {
+            //arrange
+            var abc = new InterviewPreparation();
+            //act
+            var expected = abc.Turn3digitsIntegerIntoWord(input);
+            //assert
+            Assert.True(expected == output);
+        }
+
+        [Theory]
+        [InlineData(4, new int[]{4})]
+        [InlineData(45, new int[]{4, 5})]
+        [InlineData(453921, new int[]{4, 5, 3, 9, 2, 1})]
+        public void ConvertNumberToDigits_ShouldProduceDigitsArray(int input, int[] output)
+        {
+            var abc = new InterviewPreparation();
+            var expected = abc.ConvertNumberToDigits(input);
+            Assert.True(expected.SequenceEqual(output));
+        }
+
+        [Theory]
+        [InlineData(new int[]{1,2,4}, 124)]
+        [InlineData(new int[]{1,2,4, 0, 1}, 12401)]
+        public void FormIntegerFromDigits_ShouldReturnCorrectly(int[] input, int output)
+        {
+            var abc = new InterviewPreparation();
+            var expected = abc.FormIntegerFromDigits(input);
+            Assert.True(expected == output);
+        }
+
+        [Theory]
+        [InlineData(123, "one hundred twenty three")]
+        [InlineData(1123, "one thousand one hundred twenty three")]
+        [InlineData(111123, "one hundred and eleven thousand one hundred twenty three")]
+        [InlineData(101123, "one hundred and one thousand one hundred twenty three")]
+        [InlineData(1101123, "one million one hundred and one thousand one hundred twenty three")]
+        [InlineData(1000000, "one million")]
+        [InlineData(1, "one")]
+        public void TurnNumberToWord(int input, string output)
+        {
+            var abc = new InterviewPreparation();
+            var expected = abc.TurnNumberToWord(input);
+            Assert.True(expected == output);
+        }
     }
 }
--- AlgorithmsLib/InterviewPreparation.cs
+++ AlgorithmsLib/InterviewPreparation.cs
@@ -6,6 +6,199 @@ namespace AlgorithmsLibrary
 {
     public class InterviewPreparation
     {
+       public string ReverseString(string input)
+       {
+           if (String.IsNullOrEmpty(input)) 
+                return "";
         
+           var output = "";
+           for (var i = input.Length - 1; i >= 0; i--)
+           {
+               output += input[i];
+           }
+
+           return output;
+       } 
+
+#region Number to words
+        private readonly string[] numbers_0to9 = {
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+        };
+        private readonly string[] numbers_10to19 = {
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+            "nineteen"
+        };
+        private readonly string[] numbers_Tens = {
+            "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+        };
+        private readonly string[] numbers_Hundreds = {
+            "hundred", "thousand", "million", "trillon"
+        };
+
+        public string TurnNumberToWord(int input)
+        {
+            var output = "";
+
+            int[] digits = ConvertNumberToDigits(input);
+            
+            var segment = 0;
+            var container = new Stack<int>(); //last in first out
+
+            for (var i = digits.Length-1; i >= 0; i--)
+            {
+                container.Push(digits[i]);
+
+                if (container.Count == 3 
+                    || (container.Count < 3 && i == 0))
+                {
+                    var temp = Turn3digitsIntegerIntoWord(
+                        FormIntegerFromDigits( container.ToArray()));
+                    if (segment > 0 && temp != "zero")
+                    {
+                        temp += ' ' + numbers_Hundreds[segment] + ' ';
+                    }
+                    if (temp != "zero")
+                        output = output.Insert(0, temp);
+
+                    segment++;
+                    container.Clear();
+                }
+            }
+
+
+            return output.Trim();
+        }
+
+        public string Turn3digitsIntegerIntoWord(int input)
+        {
+            //input is a number between [0, 999)
+            //output is words, such as: one hundred twenty three
+            if (input < 0) return null;
+
+            var output = "";
+            int firstDigit, secondDigit, lastDigit;
+
+            switch (digitCount(input))
+            {
+                case 1:
+                    output = numbers_0to9[input];
+                    break;
+                case 2:
+                    firstDigit = input / 10;
+                    lastDigit = input % 10;
+                    if (firstDigit == 1)
+                    {
+                        output = numbers_10to19[lastDigit];
+                    } else 
+                    {
+                        if (lastDigit == 0)
+                        {
+                            output = numbers_Tens[firstDigit - 2];
+                        } else
+                        {
+                            output = numbers_Tens[firstDigit - 2] + ' ' + numbers_0to9[lastDigit];
+                        }
+                    }      
+                    break;
+                case 3:
+                    firstDigit = input / 100;
+                    secondDigit = input / 10 % 10;
+                    lastDigit = input % 10;
+
+                    if (firstDigit == 0 && secondDigit == 0 && lastDigit == 0)
+                    {
+                        output = "";
+                        break;
+                    }
+                    if (secondDigit == 0 && lastDigit == 0)
+                    {
+                        output = numbers_0to9[firstDigit] + ' '
+                            + numbers_Hundreds[0];
+                        break;
+                    }
+                    if (secondDigit == 0 && lastDigit != 0)
+                    {
+                         output = numbers_0to9[firstDigit] + ' '
+                            + numbers_Hundreds[0] + " and " 
+                            + numbers_0to9[lastDigit];
+                        break;                       
+                    }
+                    if (secondDigit == 1)
+                    {
+                         output = numbers_0to9[firstDigit] + ' '
+                            + numbers_Hundreds[0] + " and " 
+                            + numbers_10to19[lastDigit];
+                        break;       
+                    }
+
+                    output = numbers_0to9[firstDigit] + ' '
+                            + numbers_Hundreds[0] + ' '
+                            + numbers_Tens[secondDigit - 2] + ' '
+                            + numbers_0to9[lastDigit];
+                    break;              
+            }
+
+            return output;
+        } 
+
+        public int digitCount(int num)
+        {
+            //input 56, should return 2;
+            //input 38228, should return 5;
+            var numstr = num.ToString();
+            return numstr.Length;
+        }
+       #endregion
+
+#region Regex 
+
+
+#endregion
+        // methods to find patterns in a string
+
+
+
+    public int[] ConvertNumberToDigits(int num)
+    {
+        return num.ToString().ToCharArray().Select(a => a - '0').ToArray();
+
+    }
+
+    public int FormIntegerFromDigits(int[] nums)
+    {
+        if (nums == null)
+            return -1;
+
+        var output = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var power = nums.Length - (i + 1);
+            output += nums[i]* (int)Math.Pow(10, power);
+        }
+        return output;
+    }
+
+    //https://dotnetcodr.com/2015/11/03/divide-an-integer-into-groups-with-c/
+    public static IEnumerable<int> DistributeInteger(int total, int divider)
+    {
+        if (divider == 0)
+        {
+            yield return 0;
+        }
+        else
+        {
+            int rest = total % divider;
+            double result = total / (double)divider;
+    
+            for (int i = 0; i < divider; i++)
+            {
+                if (rest-- > 0)
+                    yield return (int)Math.Ceiling(result);
+                else
+                    yield return (int)Math.Floor(result);
+            }
+        }
+    }      
+
     }
 }
